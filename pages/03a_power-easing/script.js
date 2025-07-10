908d14dc46e2b350198122ce02fd3e2f9fbbd28b
Gifty Afia Sam <samgifty523@gmail.com>
2025-07-10 21:23:23 +0100

    Update script.js

---
+++ pages/03a_power-easing/script.js
@@ -0,0 +1,17 @@
+gsap.from(".btn",{
+    y:-300,
+opacity:0,
+ease:"bounce.out",
+repeat:-1,
+
+
+})
+gsap.from(".toast",{
+    x:100,
+opacity:0,
+duration:2,
+ease:"power2.out",
+repeat:-1,
+
+
+})
